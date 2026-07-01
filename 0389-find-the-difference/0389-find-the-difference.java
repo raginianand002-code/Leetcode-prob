8c0ffaa33a9cddfd9b57b52470c79a982587b6a8
Ragini Anand <raginianand002@gmail.com>
2026-07-01 22:21:21 +0530

    Time: 2 ms (65.58%), Space: 42.8 MB (92.72%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char diff=0;
+        for(int i=0;i<s.length();i++){
+            diff^=s.charAt(i);
+        }
+            for( int i=0;i<t.length();i++){
+                diff ^=t.charAt(i);
+            }
+        
+        return diff;
+    }
+}
